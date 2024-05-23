549c41a0e1d764929ec00efd5304c290d85582ef
김지원 <kjw44200@naver.com>
2024-05-23 21:33:15 +0900

    CountryInfo ver.2

--- World_info.py
+++ World_info.py
@@ -5,7 +5,6 @@ from airflow.decorators import task
 
 
 from datetime import datetime
-from datetime import timedelta
 
 import requests
 import logging
@@ -18,29 +17,26 @@ def get_Redshift_connection(autocommit = True):
     return conn.curser()
 
 @task
-def extract(url):
-    logging.info(datetime.utcnow())
-    f = requests.get(url)
-    return f.text
-
-
-@task
-def transform(text):
-    data = json.loads(text)
+def extract_transform():
+    response = requests.get("https://restcountries.com/v3/all")
+    countries = response.json()
     records = []
-    for info in data:
-      country = info["name"]["official"]
-      population = info["population"]
-      area = info["area"]
-      records.append([country, population, area])
-    return records
+    
+    for country in countries:
+        name = country["name"]["official"]
+        population = country["population"]
+        area = country["area"]
+        
+        records.append([name, population, area])
+    
+    return records    
+
 
 @task
 def load(schema, table, records):
     logging.info("load started")    
     cur = get_Redshift_connection()   
 
-    # BEGIN과 END를 사용해서 SQL 결과를 트랜잭션으로 만들어주는 것이 좋음
     try:
         cur.execute("BEGIN;")
         cur.execute(f"DELETE FROM {schema}.World_info;")
@@ -68,6 +64,7 @@ with DAG(
     dag_id = 'world_info',
     start_date = datetime(2024,5,23),
     catchup=False,
+    tags = ['API'],
     schedule = '30 6 * * 6' #매주 토요일 6시 30분에 실행
 ) as dag:
 
@@ -75,5 +72,5 @@ with DAG(
     schema = 'chu44200'  
     table = 'World_info'
     
-    lines = transform(extract(url))
-    load(schema, table, lines)
+    results =extract_transform()
+    load(schema, table, results)
